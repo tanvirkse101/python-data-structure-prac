48a714d303f963e83d402449a587f47849a9fd7b
tanvirkse101 <tanvirkse@gmail.com>
2023-01-30 16:14:43 +0600

    bst-insert-search

---
+++ CP/BST-insertion-search.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, key):
+        self.val = key
+        self.left = None
+        self.right = None
+
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if root.val < key:
+        return search(root.right, key)
+    return search(root.left, key)
+
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+
+
+#    50
+#  /     \
+# 30     70
+#  / \ / \
+# 20 40 60 80
+
+r = Node(50)
+r = insert(r, 30)
+r = insert(r, 20)
+r = insert(r, 40)
+r = insert(r, 70)
+r = insert(r, 60)
+r = insert(r, 80)
+inorder(r)
+print(search(r, 70))
