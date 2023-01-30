c6a78a9183b01b6a87b14e238c34367e7e1e531b
tanvirkse101 <tanvirkse@gmail.com>
2023-01-30 19:40:24 +0600

    del func bst

--- CP/BST-insertion-search.py
+++ CP/BST-insertion-search.py
@@ -1,6 +1,6 @@
 class Node:
     def __init__(self, key):
-        self.val = key
+        self.key = key
         self.left = None
         self.right = None
 
@@ -9,9 +9,9 @@ def insert(root, key):
     if root is None:
         return Node(key)
     else:
-        if root.val == key:
+        if root.key == key:
             return root
-        elif root.val < key:
+        elif root.key < key:
             root.right = insert(root.right, key)
         else:
             root.left = insert(root.left, key)
@@ -19,9 +19,9 @@ def insert(root, key):
 
 
 def search(root, key):
-    if root is None or root.val == key:
+    if root is None or root.key == key:
         return root
-    if root.val < key:
+    if root.key < key:
         return search(root.right, key)
     return search(root.left, key)
 
@@ -29,8 +29,69 @@ def search(root, key):
 def inorder(root):
     if root:
         inorder(root.left)
-        print(root.val)
+        print(root.key)
         inorder(root.right)
+
+
+def minValueNode(node):
+    current = node
+
+    # loop down to find the leftmost leaf
+    while (current.left is not None):
+        current = current.left
+
+    return current
+
+# Given a binary search tree and a key, this function
+# delete the key and returns the new root
+
+
+def deleteNode(root, key):
+
+    # Base Case
+    if root is None:
+        return root
+
+    # If the key to be deleted
+    # is smaller than the root's
+    # key then it lies in  left subtree
+    if key < root.key:
+        root.left = deleteNode(root.left, key)
+
+    # If the kye to be delete
+    # is greater than the root's key
+    # then it lies in right subtree
+    elif (key > root.key):
+        root.right = deleteNode(root.right, key)
+
+    # If key is same as root's key, then this is the node
+    # to be deleted
+    else:
+
+        # Node with only one child or no child
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+
+        # Node with two children:
+        # Get the inorder successor
+        # (smallest in the right subtree)
+        temp = minValueNode(root.right)
+
+        # Copy the inorder successor's
+        # content to this node
+        root.key = temp.key
+
+        # Delete the inorder successor
+        root.right = deleteNode(root.right, temp.key)
+
+    return root
 
 
 #    50
@@ -48,3 +109,5 @@ r = insert(r, 60)
 r = insert(r, 80)
 inorder(r)
 print(search(r, 70))
+deleteNode(r, 70)
+inorder(r)
